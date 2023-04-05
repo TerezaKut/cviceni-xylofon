339f0dfeae6d8ddab7a9db1a6a1d0e6c82a2b173
TerezaKut <114390034+TerezaKut@users.noreply.github.com>
2023-04-05 16:32:12 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 console.log('Funguju')
+
+const kameny = document.querySelectorAll('.kamen');
+
+console.log(kameny);
+
+const zahrajTon = (event) => {
+    const ton = event.target.textContent;
+	const h1 = document.querySelector('h1');
+	h1.textContent = ton;
+    console.log(ton);
+    const zvuk = new Audio('tony/'+ton+'.mp3');
+    zvuk.play();
+}
+
+for (let i=0; i<kameny.length; i++){
+    kameny[i].addEventListener('click', zahrajTon);
+}
+
